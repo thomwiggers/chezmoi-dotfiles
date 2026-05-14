373f61c446f151d8dbfada8bfb444468c4b245b6
Thom Wiggers <thom@thomwiggers.nl>
2026-05-14 13:04:49 +0000

    update

--- dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
@@ -6,3 +6,36 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+
+-- go templates
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  group = vim.api.nvim_create_augroup('gotmpl_highlight', { clear = true }),
+  pattern = '*.tmpl',
+  callback = function()
+    local filename = vim.fn.expand('%:t')
+    local ext = filename:match('.*%.(.-)%.tmpl$')
+
+	-- Add more extension to syntax mappings here if you need to.
+    local ext_filetypes = {
+      go = 'go',
+      html = 'html',
+      md = 'markdown',
+      yaml = 'yaml',
+      yml = 'yaml',
+      json = 'json',
+    }
+
+    if ext and ext_filetypes[ext] then
+      -- Set the primary filetype
+      vim.bo.filetype = ext_filetypes[ext]
+
+      -- Define embedded Go template syntax
+      vim.cmd([[
+        syntax include @gotmpl syntax/gotmpl.vim
+        syntax region gotmpl start="{{" end="}}" contains=@gotmpl containedin=ALL
+        syntax region gotmpl start="{%" end="%}" contains=@gotmpl containedin=ALL
+      ]])
+    end
+  end,
+})
